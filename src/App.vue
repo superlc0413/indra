0073f4794242273d1df397542cf852ca4f557849
Charles Liu <superlc0413@163.com>
2019-11-02 11:35:20 +0800

    改造成基于element-ui

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,5 @@ export default {
 
 <style lang="scss" scoped>
 @import "./common/base.scss";
-@import "./common/style.scss";
 </style>
 
--- src/card/index.vue
+++ src/card/index.vue
@@ -1,19 +1,44 @@
 <template>
-  <div>Hello World</div>
+  <div style="height:500px">
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
+  </div>
 </template>
+
 <script>
 export default {
-  data() {
-    return {};
+  created() {
+    console.log(this.tableData);
+    debugger;
   },
-  methods: {},
-  computed: {},
-  created() {},
-  mounted() {}
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
+    };
+  }
 };
 </script>
-<style lang="scss" scoped>
-@import "../common/base.scss";
-</style>
-
-
